670cc46958be41ccb6c11cd30c4f9f2080f55da0
Martin Wetterwald <martin@wetterwald.eu>
2014-12-17 17:28:57 +0800

    Implements Vod::NetEvents classes.

---
+++ src/Vod/Vod_NetEvents.cpp
@@ -0,0 +1,25 @@
+#include "Vod_NetEvents.hpp"
+
+namespace Vod
+{
+    namespace NetEvent
+    {
+        EventMapping::EventMapping ( )
+        {
+            object = nullptr;
+        }
+
+        EventMapping::~EventMapping ( )
+        {
+        }
+
+        ChooseSubscriptionMapping::ChooseSubscriptionMapping ( )
+        {
+            object = nullptr;
+        }
+
+        ChooseSubscriptionMapping::~ChooseSubscriptionMapping ( )
+        {
+        }
+    }
+}
---
+++ src/Vod/Vod_NetEvents.hpp
@@ -0,0 +1,85 @@
+#ifndef _VOD_NET_EVENTS_HPP_
+#define _VOD_NET_EVENTS_HPP_
+
+#include <SocketIOEvent/SocketIOEvent_Event.hpp>
+
+namespace Vod
+{
+    namespace NetEvent
+    {
+        class thiscall
+        {
+        };
+
+        class EventMapping
+        {
+        public:
+            template < class CLASS1, class CLASS2 >
+            inline void map
+            (
+                CLASS1 * pobject,
+                void ( CLASS1::* pfunction ) ( CLASS2 * )
+            )
+            {
+                object = ( thiscall * ) pobject;
+                function = ( bool ( thiscall::* ) ( void * ) ) pfunction;
+            }
+
+            template < class CLASS1 >
+            bool operator ( ) ( CLASS1 * socket )
+            {
+                if ( object )
+                {
+                    return ( ( * object ).* function ) ( socket );
+                }
+                return false;
+            }
+
+            EventMapping ( );
+            virtual ~EventMapping ( );
+
+
+        protected:
+            thiscall * object;
+            bool ( thiscall::* function ) ( void * );
+
+
+
+        };
+
+        class ChooseSubscriptionMapping
+        {
+        public:
+            template < class CLASS1, class CLASS2 >
+            inline void map
+            (
+                CLASS1 * pobject,
+                void ( CLASS1::* pfunction ) ( CLASS2 *, NetFlux::SocketIOEvent::Event & )
+            )
+            {
+                object = ( thiscall * ) pobject;
+                function = ( void ( thiscall::* ) ( void *, NetFlux::SocketIOEvent::Event &event ) ) pfunction;
+            }
+
+
+            template < class CLASS1 >
+            void operator ( ) ( CLASS1 * socket, NetFlux::SocketIOEvent::Event & e )
+            {
+                if ( object )
+                {
+                    ( ( * object ).* function ) ( socket, e );
+                }
+            }
+
+            ChooseSubscriptionMapping ( );
+            virtual ~ChooseSubscriptionMapping ( );
+
+
+        protected:
+            thiscall * object;
+            void ( thiscall::* function ) ( void *, NetFlux::SocketIOEvent::Event & );
+        };
+    }
+}
+
+#endif
